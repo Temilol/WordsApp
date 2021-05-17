8ef16ef81eeb79df1b9c5934ead95d482830eb12
Temilola <temz4rill@gmail.com>
2021-05-17 13:58:17 -0400

    Completed logic to display views and navigate between screens

--- app/src/main/java/com/example/wordsapp/MainActivity.kt
+++ app/src/main/java/com/example/wordsapp/MainActivity.kt
@@ -1,11 +1,62 @@
 package com.example.wordsapp
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.wordsapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var recyclerView: RecyclerView
+    private var isLinearLayoutManager = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        recyclerView = binding.recycleView
+        configureLayout()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.layout_menu, menu)
+        val layoutButton = menu?.findItem(R.id.action_switch_layout)
+        setIcon(layoutButton)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_switch_layout -> {
+                isLinearLayoutManager = !isLinearLayoutManager
+                configureLayout()
+                setIcon(item)
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun configureLayout() {
+        if (isLinearLayoutManager) {
+            recyclerView.layoutManager = LinearLayoutManager(this)
+        } else {
+            recyclerView.layoutManager = GridLayoutManager(this, 4)
+        }
+        recyclerView.adapter = LetterAdapter()
+    }
+
+    private fun setIcon(menuItem: MenuItem?) {
+        if (menuItem == null) return
+        menuItem.icon = when (isLinearLayoutManager) {
+            true -> ContextCompat.getDrawable(this, R.drawable.ic_grid_layout)
+            else -> ContextCompat.getDrawable(this, R.drawable.ic_linear_layout)
+        }
     }
 }
---
+++ app/src/main/java/com/example/wordsapp/WordAdapter.kt
@@ -0,0 +1,88 @@
+package com.example.wordsapp
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.accessibility.AccessibilityNodeInfo
+import android.widget.Button
+import androidx.annotation.RequiresApi
+import androidx.recyclerview.widget.RecyclerView
+
+class WordAdapter(private val letterId: String, context: Context) :
+    RecyclerView.Adapter<WordAdapter.WordViewHolder>() {
+
+    private val filteredWords: List<String>
+
+    init {
+        // Retrieve the list of words from res/values/arrays.xml
+        val words = context.resources.getStringArray(R.array.words).toList()
+
+        filteredWords = words
+            .filter { it.startsWith(letterId, ignoreCase = true) }
+            .shuffled()
+            .take(5)
+            .sorted()
+    }
+
+    class WordViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+        val button: Button = view.findViewById(R.id.button_item)
+    }
+
+    override fun getItemCount(): Int = filteredWords.size
+
+    /**
+     * Creates new views with R.layout.item_view as its template
+     */
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WordViewHolder {
+        val layout = LayoutInflater
+            .from(parent.context)
+            .inflate(R.layout.item_view, parent, false)
+
+        // Setup custom accessibility delegate to set the text read
+        layout.accessibilityDelegate = Accessibility
+        return WordViewHolder(layout)
+    }
+
+    /**
+     * Replaces the content of an existing view with new data
+     */
+    override fun onBindViewHolder(holder: WordViewHolder, position: Int) {
+        val item = filteredWords[position]
+        // Needed to call startActivity
+        val context = holder.view.context
+        holder.button.text = item
+        holder.button.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW).apply {
+                data = Uri.parse("${DetailActivity.SEARCH_PREFIX}${item}")
+            }
+            context.startActivity(intent)
+        }
+    }
+
+    // Setup custom accessibility delegate to set the text read with
+    // an accessibility service
+    companion object Accessibility : View.AccessibilityDelegate() {
+        @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+        override fun onInitializeAccessibilityNodeInfo(
+            host: View?,
+            info: AccessibilityNodeInfo?
+        ) {
+            super.onInitializeAccessibilityNodeInfo(host, info)
+            // With `null` as the second argument to [AccessibilityAction], the
+            // accessibility service announces "double tap to activate".
+            // If a custom string is provided,
+            // it announces "double tap to <custom string>".
+            val customString = host?.context?.getString(R.string.look_up_word)
+            val customClick =
+                AccessibilityNodeInfo.AccessibilityAction(
+                    AccessibilityNodeInfo.ACTION_CLICK,
+                    customString
+                )
+            info?.addAction(customClick)
+        }
+    }
+}
